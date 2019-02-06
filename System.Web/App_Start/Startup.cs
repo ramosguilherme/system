28556a8e3a2ed087b303002e076c65cb7b5116a1
Guilherme Ramos <guimeloramos@hotmail.com>
2019-02-06 00:40:36 -0200

    Commit initial

---
+++ System.Web/App_Start/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(System.Web.App_Start.Startup))]
+
+namespace System.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.Use((context, next) =>
+            {
+                TextWriter output = context.Get<TextWriter>("host.TraceOutput");
+                return next().ContinueWith(result =>
+                {
+                    output.WriteLine("Scheme {0} : Method {1} : Path {2} : MS {3}",
+                    context.Request.Scheme, context.Request.Method, context.Request.Path, getTime());
+                });
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(getTime() + " My First OWIN App");
+            });
+        }
+
+        string getTime()
+        {
+            return DateTime.Now.Millisecond.ToString();
+        }
+    }
+}
